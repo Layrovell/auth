cca70f21a61bc7fe98ea53d8caab8d8206741ff5
Marina Prodan <marinap@sweetsoft.com.ua>
2021-03-26 18:54:36 +0200

    refactored core functionality

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
     const [repeatPasswordError, setRepeatPasswordError] = useState(false);
 
     const [formValid, setFormValid] = useState(false);
-    const [showCongrats, setShowCongrats] = useState(false);
+    const [formValidMessage, setFormValidMessage] = useState('');
 
     const blurHandler = (e) => {
         switch (e.target.name) {
@@ -56,190 +56,186 @@ function App() {
         }
     }
 
-    const nameHandler = (e) => {
-        setName(e.target.value);
-        const regexp = /^[a-zA-Z\u00C0-\u00ff]+$/;
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-        if (!regexp.test(String(e.target.value).toLowerCase())) {
+        // validation for name
+        const reName = /^[a-zA-Z\u00C0-\u00ff]+$/;
+        const validName = reName.test(name);
+
+        if (name.length === 0) {
+            setNameError(true);
+        } else if (!validName) {
             setNameErrorMessage('Некоректное имя');
         } else {
             setNameErrorMessage('');
         }
-    }
 
-    const surnameHandler = (e) => {
-        setSurname(e.target.value);
-        const regexp = /^[a-zA-Z\u00C0-\u00ff]+$/;
+        // validation for surname
+        const reSurname = /^[a-zA-Z\u00C0-\u00ff]+$/;
+        const validSurname = reSurname.test(surname);
 
-        if (!regexp.test(String(e.target.value).toLowerCase())) {
+        if (surname.length === 0) {
+            setSurnameError(true);
+        } else if (!validSurname) {
             setSurnameErrorMessage('Некоректная фамилия');
         } else {
             setSurnameErrorMessage('');
         }
-    }
 
-    const emailHandler = (e) => {
-        setEmail(e.target.value);
-        const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        // validation for email
+        const reEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const validEmail = reEmail.test(email);
 
-        if (!regexp.test(String(e.target.value).toLowerCase())) {
+        if (email.length === 0) {
+            setEmailError(true);
+        } else if (!validEmail) {
             setEmailErrorMessage('Недопустимый формат почты');
         } else {
             setEmailErrorMessage('');
         }
 
-        if (e.target.value.length === 0) {
-            setPasswordErrorMessage('Почта не может быть пустой');
-        }
-    }
-
-    const dateHandler = (e) => {
-        setDate(e.target.value);
+        // validation for date
         const limit = 16;
 
         const dateNow = new Date().toISOString().split('T')[0];
-        let currentAge = (new Date(dateNow).getTime() - new Date(e.target.value).getTime()) / 1000 / 60 / 60 / 24;
+        let currentAge = (new Date(dateNow).getTime() - new Date(date).getTime()) / 1000 / 60 / 60 / 24;
         currentAge = Math.round(currentAge / 365);
 
-        if (currentAge < limit) {
+        if (date.length === 0) {
+            setDateError(true);
+        } else if (currentAge < limit) {
             setDateErrorMessage('Возраст пользователя меньше 16');
         } else {
             setDateErrorMessage('');
         }
-    }
 
-    const loginHandler = (e) => {
-        setLogin(e.target.value);
-        const regexp = /^[0-9a-zA-Z]+$/;
+        // validation for login
+        const reLogin = /^[0-9a-zA-Z]+$/;
+        const validLogin = reLogin.test(login);
 
-        if (!regexp.test(String(e.target.value))) {
+        if (login.length === 0) {
+            setLoginError(true);
+        } else if (!validLogin) {
             setLoginErrorMessage('Недопустимый формат логина');
         } else {
             setLoginErrorMessage('');
         }
-    }
 
-    const passwordHandler = (e) => {
-        setPassword(e.target.value);
-        const regexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/;
+        // validation for password
+        const rePassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/;
+        const validPassword = rePassword.test(password);
 
-        if (!regexp.test(String(e.target.value))) {
+        if (password.length === 0) {
+            setPasswordError(true);
+        } else if (!validPassword) {
             setPasswordErrorMessage('Неправильный формат пароля');
-        } else if (e.target.value.length < 6) {
+        } else if (password.length < 6) {
             setPasswordErrorMessage('Пароль не должен быть менее 6 символов');
         } else {
             setPasswordErrorMessage('');
         }
 
-        if (e.target.value.length === 0) {
-            setPasswordErrorMessage('Пароль не может быть пустым');
-        }
-    }
-
-    const repeatPasswordHandler = (e) => {
-        setRepeatPassword(e.target.value);
-
-        if (password !== repeatPassword) {
-            setRepeatPasswordErrorMessage('password do not match!');
-        }
-        console.log(password === repeatPassword);
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!nameError && !surnameError && !emailError && !dateError && loginError && passwordError) {
-            setFormValid(true);
+        // validation for repeat password
+        const validRepeatPassword = password === repeatPassword;
+        if (repeatPassword.length === 0) {
+            setRepeatPasswordError(true);
+        } else if (password !== repeatPassword) {
+            setRepeatPasswordErrorMessage('Пароль введен не верно!');
         } else {
-            setFormValid(false);
+            setRepeatPasswordErrorMessage('');
+        }
+
+        if (validName && validSurname && validEmail && (limit === currentAge) && validLogin && validPassword && validRepeatPassword) {
+            setFormValid(true);
+            setFormValidMessage('Success!');
+            console.log('Success');
         }
     }
 
     return (
         <div className="App">
-            <form
-                onSubmit={handleSubmit}
-                className='register-form'
-            >
+            <form onSubmit={handleSubmit} className='register-form'>
 
                 <input
                     value={name}
                     onBlur={(e) => blurHandler(e)}
-                    onChange={(e) => nameHandler(e)}
+                    onChange={(e) => setName(e.target.value)}
                     type="text"
                     placeholder="Name"
                     name="name"
+                    className={nameError ? 'warn' : 'test'}
                 />
-                {(nameError && nameErrorMessage && <div className="error-message">{nameErrorMessage}</div>)}
+                {(nameError && nameErrorMessage &&
+                    <div className="error-message">{nameErrorMessage}</div>)}
 
                 <input
                     value={surname}
                     onBlur={(e) => blurHandler(e)}
-                    onChange={(e) => surnameHandler(e)}
+                    onChange={(e) => setSurname(e.target.value)}
                     type="text"
                     placeholder="Surname"
                     name="surname"
                 />
-                {(surnameError && surnameErrorMessage && <div className="error-message">{surnameErrorMessage}</div>)}
+                {(surnameError && surnameErrorMessage &&
+                    <div className="error-message">{surnameErrorMessage}</div>)}
 
                 <input
                     value={email}
                     onBlur={(e) => blurHandler(e)}
-                    onChange={(e) => emailHandler(e)}
+                    onChange={(e) => setEmail(e.target.value)}
                     type="text"
                     placeholder="Email"
                     name="email"
                 />
-                {(emailError && emailErrorMessage && <div className="error-message">{emailErrorMessage}</div>)}
+                {(emailError && emailErrorMessage &&
+                    <div className="error-message">{emailErrorMessage}</div>)}
 
                 <input
                     value={date}
                     onBlur={(e) => blurHandler(e)}
-                    onChange={(e) => dateHandler(e)}
+                    onChange={(e) => setDate(e.target.value)}
                     type="date"
                     name="date"
                 />
-                {(dateError && dateErrorMessage && <div className="error-message">{dateErrorMessage}</div>)}
+                {(dateError && dateErrorMessage &&
+                    <div className="error-message">{dateErrorMessage}</div>)}
 
                 <input
                     value={login}
                     onBlur={(e) => blurHandler(e)}
-                    onChange={(e) => loginHandler(e)}
+                    onChange={(e) => setLogin(e.target.value)}
                     type="text"
                     placeholder="Login"
                     name="login"
                 />
-                {(loginError && loginErrorMessage && <div className="error-message">{loginErrorMessage}</div>)}
+                {(loginError && loginErrorMessage &&
+                    <div className="error-message">{loginErrorMessage}</div>)}
 
                 <input
                     value={password}
                     onBlur={(e) => blurHandler(e)}
-                    onChange={(e) => passwordHandler(e)}
+                    onChange={(e) => setPassword(e.target.value)}
                     type="password"
                     placeholder="Password"
                     name="password"
                 />
-                {(passwordError && passwordErrorMessage && <div className="error-message">{passwordErrorMessage}</div>)}
+                {(passwordError && passwordErrorMessage &&
+                    <div className="error-message">{passwordErrorMessage}</div>)}
 
                 <input
                     onBlur={(e) => blurHandler(e)}
                     value={repeatPassword}
-                    onChange={(e) => repeatPasswordHandler(e)}
+                    onChange={(e) => setRepeatPassword(e.target.value)}
                     type="password"
                     placeholder="Repeat Password"
                     name="repeatPassword"
                 />
-                {(repeatPasswordError && repeatPasswordErrorMessage && <div className="error-message">{repeatPasswordErrorMessage}</div>)}
+                {(repeatPasswordError && repeatPasswordErrorMessage &&
+                    <div className="error-message">{repeatPasswordErrorMessage}</div>)}
 
-                <button
-                    // disabled={!formValid}
-                    onClick={(e) => setShowCongrats(true)}
-                    type='submit'
-                    className="btn"
-                >
-                    Register
-                </button>
-
-                {formValid && <div className="success-message">Регистрация прошла успешно!</div>}
+                <button type='submit' className="btn">Register</button>
+                {formValid && <div className="success-message">{formValidMessage}</div>}
             </form>
         </div>
     );
